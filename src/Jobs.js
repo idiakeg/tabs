a3333719abf29c2c6494aeb6da6198731a2b6dfb
Godspower Idiakose <idiakeg@gmail.com>
2022-09-12 02:41:57 +0100

    included styles

--- src/Jobs.js
+++ src/Jobs.js
@@ -2,13 +2,16 @@ import React from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 
 const Jobs = ({ job, value, handleClick }) => {
-	// the company, title,dates and duties that be displayed to the user is dependent on the "value" variable passed in from app.js. this value will be changeable upon the click of a button that changes the value variable, causing a rerender of rthe page with the correct data
 	const { company, title, dates, duties } = job[value];
 	return (
 		<div className="job__container">
 			<div className="button__container">
 				{job.map(({ id, company }, index) => (
-					<button onClick={() => handleClick(index)} key={id}>
+					<button
+						className={index === value ? "active" : undefined}
+						onClick={() => handleClick(index)}
+						key={id}
+					>
 						{company}
 					</button>
 				))}
@@ -17,14 +20,18 @@ const Jobs = ({ job, value, handleClick }) => {
 				<p className="title">{title}</p>
 				<p className="company">{company}</p>
 				<p className="dates">{dates}</p>
-				{duties.map((duty, index) => {
-					return (
-						<div className="duty" key={index}>
-							<FaAngleDoubleRight />
-							<p className="duty__text">{duty}</p>
-						</div>
-					);
-				})}
+				<section className="duty__container">
+					{duties.map((duty, index) => {
+						return (
+							<div className="duty" key={index}>
+								<span className="icon">
+									<FaAngleDoubleRight />
+								</span>
+								<p className="duty__text">{duty}</p>
+							</div>
+						);
+					})}
+				</section>
 			</div>
 		</div>
 	);
